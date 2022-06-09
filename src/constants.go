e2329bb18e0641f0a4f063215c9088161c0be344
Gavin <tianyaogavin@gmail.com>
2022-06-09 21:24:14 +0800

    add tx type const

---
+++ src/constants.go
@@ -0,0 +1,24 @@
+package src
+
+const (
+	TxTypeEmpty = iota
+	TxTypeRegisterZns
+	TxTypeCreatePair
+	TxTypeUpdatePairRate
+	TxTypeDeposit
+	TxTypeDepositNft
+	TxTypeTransfer
+	TxTypeSwap
+	TxTypeAddLiquidity
+	TxTypeRemoveLiquidity
+	TxTypeWithdraw
+	TxTypeCreateCollection
+	TxTypeMintNft
+	TxTypeTransferNft
+	TxTypeAtomicMatch
+	TxTypeCancelOffer
+	TxTypeWithdrawNft
+	TxTypeFullExit
+	TxTypeFullExitNft
+	TxTypeOffer
+)
