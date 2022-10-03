9612c85bfde9468485ba42b059158dc75f76c7fb
carlcarl <carl.h@binance.com>
2022-10-03 18:41:08 +0800

    feat: add totalAssetPrice in GetAccount API response

--- types/type.go
+++ types/type.go
@@ -13,12 +13,13 @@ type AccountAsset struct {
 }
 
 type Account struct {
-	Status uint32          `json:"status"`
-	Index  int64           `json:"index"`
-	Name   string          `json:"name"`
-	Pk     string          `json:"pk"`
-	Nonce  int64           `json:"nonce"`
-	Assets []*AccountAsset `json:"assets"`
+	Status          uint32          `json:"status"`
+	Index           int64           `json:"index"`
+	Name            string          `json:"name"`
+	Pk              string          `json:"pk"`
+	Nonce           int64           `json:"nonce"`
+	Assets          []*AccountAsset `json:"assets"`
+	TotalAssetPrice string          `json:"total_asset_price"`
 }
 
 type SimpleAccount struct {
